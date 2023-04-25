afda08e97d027721488c9248fce2971e0c6eb8fe
RSBalchII <128771311+RSBalchII@users.noreply.github.com>
2023-04-24 23:11:25 -0600

    added two players and made 5 errors to be amended next session  Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-let canvas;
-canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle = "white"
-c.fillRect(10, 10, 50, 50)
+class Player {
+     contructor{
+      this.position = {
+        x:0,
+        y:0,
+     }
+    }
+    draw(){
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, 100, 100)
+    }
+}
+let y = 100
+let y2 = 100
+function animate() {
+    window.requestIdleCallback(animate)
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
 
+    c.fillStyle = 'red'
+    c.fillRect(200, y, 100, 100)
+    y++
+
+    c.fillStyle = 'red'
+    c.fillRect(400, y2, 100, 100)
+    y2++
+}
+animate()
